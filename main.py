5ec50f6414f17ed0653df5c9a5f5b46a009d4862
Ayaz Namazov <ayaznamazov019@gmail.com>
2024-09-05 12:59:49 +0300

    Feat: basic start func in bot

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print('Hello world')
-print('sosi hyi')
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_first_name = update.message.from_user.first_name
+    await update.message.reply_text(f'Hello, {user_first_name}!')
+    
+if __name__ == '__main__':
+    # Make sure to replace your token with a valid one
+    application = ApplicationBuilder().token('7079751398:AAHzb-4mKMqzIqduRKhXkWSfTydj5UIT5Aw').build()
+    application.add_handler(CommandHandler('start', start))
+    application.run_polling()
+
+
+# измененный файл аязом
